ed69ae130628f368ccdd49061130beb25fe47fe9
josip <josipkeresman@gmail.com>
2025-02-06 23:22:19 +0100

    Improve naming in Facade impl

--- src/com/keresman/gof/facade/Main.java
+++ src/com/keresman/gof/facade/Main.java
@@ -1,10 +1,11 @@
 package com.keresman.gof.facade;
 
 public class Main {
-    public static void main(String[] args) {
-        CryptoFacade buyCrypto = new CryptoFacade();
 
-        buyCrypto.buyCrypto(1000, "BTC");
-        buyCrypto.buyCrypto(1_000_000, "ETH");
+    public static void main(String[] args) {
+        CryptoFacade buyCryptoFacade = new CryptoFacade();
+        buyCryptoFacade.buyCrypto(1000, "BTC");
+        buyCryptoFacade.buyCrypto(1_000_000, "ETH");
     }
+
 }
